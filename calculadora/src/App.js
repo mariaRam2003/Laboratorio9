54f2ea1467f167649bb9542fc3a08318e16b4cde
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-05-29 14:18:58 -0600

    limitar a 9 el display

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,11 +1,95 @@
 import './App.css';
+import Button from "./components/Button";
+import Display from './components/Display';
+import ButtonClear from './components/ButtonClear';
+import {useState} from 'react';
+import { useEffect } from 'react';
+import {evaluate, prodDependencies} from 'mathjs';
 
-function App() {
-  return(
-    <div className='App'>
-      
-    </div>  
-  )
-}
 
-export default App;
+  function App() {
+
+    const [input, setInput] = useState('');
+
+    const agregarInput = val => {
+      if (input.length < 9) {
+        setInput(prevInput => prevInput + val);
+      }
+    };
+
+    const calcularResultado = () => {
+      let resultado;
+      if(input){
+        resultado = evaluate(input);
+        setInput(Number(resultado).toFixed(8));
+      }else{
+        alert("Por favor ingrese valores para realizar los calculos");
+      }
+    };
+
+    useEffect(() => {
+      const handleKeyDown = event => {
+        const { key } = event;
+        if (/[0-9]/.test(key) && input.length < 9) {
+          agregarInput(key);
+        } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+          agregarInput(key);
+        } else if (key === '=') {
+          calcularResultado();
+        }
+      };
+  
+      document.addEventListener('keydown', handleKeyDown);
+  
+      return () => {
+        document.removeEventListener('keydown', handleKeyDown);
+      };
+    }, []);
+  
+
+    return (
+      <div className="App">
+
+          <div className='contenedor-calculadora'>
+            <Display input={input}></Display>
+
+            <div className='fila'>
+              <Button manejarClic={agregarInput}>1</Button>
+              <Button manejarClic={agregarInput}>2</Button>
+              <Button manejarClic={agregarInput}>3</Button>
+              <Button manejarClic={agregarInput}>+</Button>
+
+            </div>
+            <div className='fila'>
+              <Button manejarClic={agregarInput}>4</Button>
+              <Button manejarClic={agregarInput}>5</Button>
+              <Button manejarClic={agregarInput}>6</Button>
+              <Button manejarClic={agregarInput}>-</Button>
+
+            </div>
+            <div className='fila'>
+              <Button manejarClic={agregarInput}>7</Button>
+              <Button manejarClic={agregarInput}>8</Button>
+              <Button manejarClic={agregarInput}>9</Button>
+              <Button manejarClic={agregarInput}>*</Button>
+
+            </div>
+            <div className='fila'>
+              <Button manejarClic={calcularResultado}>=</Button>
+              <Button manejarClic={agregarInput}>0</Button>
+              <Button manejarClic={agregarInput}>.</Button>
+              <Button manejarClic={agregarInput}>/</Button>
+
+            </div>
+            <div className='fila'>
+              <ButtonClear manejarClear={() => setInput('')}>Clear</ButtonClear>
+
+            </div>
+
+          </div>
+
+      </div>
+    );
+  }
+
+  export default App;
